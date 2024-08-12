1fc1588b857a38c552d69026f5380bf2742df778
yonatanmengesha <yonatan.mengesha2017@gmail.com>
2024-08-11 22:06:57 -0400

    commit after some change of code

---
+++ src/app/constants/url.ts
@@ -0,0 +1,6 @@
+ export const API_URL_RL = 'http://localhost:9091';//'http://10.0.0.104:FOOD-CATALOGUE-SERVICE:9092';////  //
+export const API_URL_Order = 'http://localhost:9094';//'http://10.0.0.104:ORDER-SERVICE:9094';//////
+export const API_URL_FC =  'http://localhost:9092';//'http:// 10.0.0.104:RESTAURANT-SERVICE:9091';//////'http://localhost:9092';
+ export const API_URL_UD = 'http://localhost:9093';//'http:// 10.0.0.104:USER-SERVICE:9093';//////
+//export const K8ExternalIp = 'http://k8s-default-awsingre-3d4b7f90d4-122545836.eu-west-3.elb.amazonaws.com';
+
